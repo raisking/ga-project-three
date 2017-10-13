e6d6f890d1f78b115d184577ba0f7ae9b608a170
raisking <raiprakas007@gmail.com>
2017-10-13 17:49:35 -0400

    set up node and postinstall

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(bodyParser.json());
 app.get('/', (req,res) => {
   res.send('Hello world!')
 })
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
